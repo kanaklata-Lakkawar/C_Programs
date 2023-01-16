7c13dea04de8ec654e8eb7b33d4e4ab3065e57b8
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-01-16 07:53:14 +0530

    Add files via upload
    C program to check whether the Number is positive or not

---
+++ Conditional Statements/assignment_3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num;
+    printf(" Enter any num ");
+    scanf(" %d",&num);
+    if(num>0)
+          printf(" The Number is Positive");
+     else
+          printf(" The Number is Negative");
+
+    return 0;
+}
