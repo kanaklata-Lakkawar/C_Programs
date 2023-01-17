494e8ef9bf0f829518a040b1511043c4012dee0c
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-01-17 10:28:32 +0530

    Add files via upload
    C program to check whether the given year is leap year or not

---
+++ Conditional Statements/assignment_4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int year;
+    printf(" Enter any year\n");
+    scanf(" %d",&year);
+    if (year%4==0 && year%100!=0 || year%400==0)
+        printf(" %d year is Leap Year",year);
+        else
+    printf(" %d year is not a Leap Year",year);
+    return 0;
+}
