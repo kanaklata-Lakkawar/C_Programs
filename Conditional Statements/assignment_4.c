b1e131555aaa1a87f31eb4f51042c0b41848df14
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-01-17 10:32:33 +0530

    Update assignment_4.c

--- Conditional Statements/assignment_4.c
+++ Conditional Statements/assignment_4.c
@@ -6,6 +6,7 @@ int main()
     int year;
     printf(" Enter any year\n");
     scanf(" %d",&year);
+    
     if (year%4==0 && year%100!=0 || year%400==0)
     {
      printf(" %d year is Leap Year",year);
