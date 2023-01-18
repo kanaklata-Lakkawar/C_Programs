6d762174c2317f7010b5a8d35fe1d79a3b6e1e37
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-01-18 08:20:51 +0530

    Update assignment_5.c

--- Conditional Statements/assignment_5.c
+++ Conditional Statements/assignment_5.c
@@ -7,7 +7,10 @@ int main()
     printf(" Enter Your age ");
     scanf("%d",&age);
     if(age>=18)
-        printf(" Congratulations!! you are Eligilble for casting your vote");
+    {
+    printf(" Congratulations!! you are Eligilble for casting your vote");
+    }
+        
     else
        printf(" Sorry you are not eligible for vote");
     return 0;
