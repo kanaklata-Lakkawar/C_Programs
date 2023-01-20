714cd8e23aafbfea279d2969fdad1663d548faea
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-01-20 10:23:09 +0530

    Update assignment_7.c

--- Conditional Statements/assignment_7.c
+++ Conditional Statements/assignment_7.c
@@ -4,8 +4,11 @@
 int main()
 {
     int a,b,c;
+    printf(" Enter 1st Number=\n");
     scanf("%d",&a);
+     printf(" Enter 2nd Number=\n");
     scanf("%d",&b);
+     printf(" Enter 3rd Number=\n");
     scanf("%d",&c);
     printf(" 1st Number=12\n");
     printf(" 2nd Number=25\n");
