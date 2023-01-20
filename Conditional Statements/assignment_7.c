dd9748171259aa97b3da8f8a1b94bc8398750168
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-01-20 10:19:27 +0530

    Add files via upload

---
+++ Conditional Statements/assignment_7.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a=12,b=25,c=52;
+    printf(" 1st Number=12\n");
+    printf(" 2nd Number=25\n");
+    printf(" 3rd Number=52\n");
+    if(a>b)
+    {
+        if(a>c)
+            printf(" 1st Number is greatest amongst three");
+    }
+    if(b>a)
+    {
+        if(b>c)
+             printf(" 2nd Number is greatest amongst three");
+    }
+    if(c>a)
+    {
+        if(c>b)
+            printf(" 3rd Number is greatest amongst three");
+    }
+    return 0;
+}
