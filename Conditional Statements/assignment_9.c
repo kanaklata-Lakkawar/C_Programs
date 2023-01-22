676bff67dad62a99cc0cad845eaa77f87c7f92db
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-01-22 12:08:30 +0530

    Add files via upload

---
+++ Conditional Statements/assignment_9.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  float phy,chem,compApp,total,percentage;
+    printf("Enter Physics Marks = ");
+    scanf("%f",&phy);
+    printf("Enter Chemistry Marks = ");
+     scanf("%f",&chem);
+    printf("Enter Maths Marks = ");
+     scanf("%f",&compApp);
+    total=phy+chem+compApp;
+    printf("Roll No : 784\n");
+    printf("Name of Student : James\n");
+    printf("Total = %f",total);
+    percentage=(total/300)*100;
+    printf("Percentage = %f\n",percentage);
+    if(percentage>=70 && percentage<=100)
+    printf("Division = First\n");
+    if(percentage>=50 && percentage<=70)
+    printf("Division = Second\n");
+    else
+            printf("Division = Third\n");
+    return 0;
+}
