23ae219f69c52b87e782ffaad0b95f33b10e57cb
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-01-23 10:21:44 +0530

    Add files via upload
    C program to check whether  a character is an alphabet, digit or special character

---
+++ Conditional Statements/assignment_10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char ch;
+    printf("Enter any Key : ");
+    scanf("%c",&ch);
+     if(ch>='a' && ch<='z')
+        printf("%c is Alphabet",ch);
+     else if(ch>='A' && ch<='Z')
+        printf("%c is Alphabet",ch);
+           else if(ch>=48 && ch<=57)
+            printf("%c is Digit",ch);
+               else
+                printf("%c is Special Character",ch);
+    return 0;
+}
