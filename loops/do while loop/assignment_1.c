469ba4ba0971e21c0b41d120493bc2717557dc84
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-01-25 08:03:48 +0530

    Add files via upload

---
+++ loops/do while loop/assignment_1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i;
+    printf("The First 10 Natural Numbers are\n");
+    for(i=1;i<11;i++)
+    printf("%d\t",i);
+
+    return 0;
+}
+/*printf("The First 10 Natural Numbers are\n");
+    for(i=1;i<11;i++)
+    printf("%d\t",i);
+
+    #while
+        {
+    int i;
+    printf("The First 10 Natural Numbers are\n");
+    while(i<10)
+    {
+        i++;
+     printf("%d\t",i);
+    }
+
+    Do while
+    {
+    int i;
+    printf("The First 10 Natural Numbers are\n");
+    do
+
+    {
+        i++;
+     printf("%d\t",i);
+    }while(i<10);
+    }
+
+*/
