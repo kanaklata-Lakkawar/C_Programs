8a6d0cd27e8f52a5cd802562fa0be1624333f4d7
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-01-25 08:04:48 +0530

    Update assignment_1.c

--- loops/do while loop/assignment_1.c
+++ loops/do while loop/assignment_1.c
@@ -3,11 +3,7 @@
 
 int main()
 {
-    int i;
-    printf("The First 10 Natural Numbers are\n");
-    for(i=1;i<11;i++)
-    printf("%d\t",i);
-
+   
     return 0;
 }
 /*printf("The First 10 Natural Numbers are\n");
