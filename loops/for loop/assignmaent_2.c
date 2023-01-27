ab449bcddae96bc2414328d6575cc5940285efb8
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-01-27 10:39:50 +0530

    Update assignmaent_2.c

--- loops/for loop/assignmaent_2.c
+++ loops/for loop/assignmaent_2.c
@@ -4,13 +4,13 @@
 int main()
 {
     int i=1,sum=0;
-    while(i<8)
+  for(i=0;i<8;i++)
     {
     printf(" %d\n",i);
     sum=sum+i;
-
-    i++;
-    }
 printf(" The Sum of 7 Natural Numbers upto 7 terms : %d\n",sum);
+  
+    }
+
     return 0;
 }
