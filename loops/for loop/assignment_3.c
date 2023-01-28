b952b2c24bf3847fb48d0ebc27e65666834cef68
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-01-28 11:45:52 +0530

    Add files via upload

---
+++ loops/for loop/assignment_3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i=0,sum=0;
+    float avg=0;
+    while(i<11)
+    {
+    printf("%d\n",i);
+    i++;
+    sum+=i;
+    avg=sum/10.0;
+    }
+    printf("The sum of Numbers is : %d\n",sum);
+    printf("The Average is : %f\n",avg);
+    return 0;
+}
