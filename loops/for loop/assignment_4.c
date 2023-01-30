4d4ee49492195bf3baec700bdf3c550824f6b941
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-01-30 07:57:16 +0530

    Add files via upload
    C program to display the cube of the number upto given an integer

---
+++ loops/for loop/assignment_4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i=1;
+    while(i<6)
+    {
+    printf("Number is : %d and cube of %d is : %d\n",i,i,i*i*i);
+    i++;
+    }
+    return 0;
+}
