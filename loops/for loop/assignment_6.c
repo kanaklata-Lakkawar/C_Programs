6a5fc0e2cd70095e893926df39e00bc5e9ea2277
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-02-01 10:09:30 +0530

    Add files via upload
    C program to display the multiplication table number starting from 1  to  8

---
+++ loops/for loop/assignment_6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,j;
+    for(i=1;i<11;i++)
+    {
+      for(j=1;j<11;j++)
+        printf("%d X %d = %d\n",i,j,i*j);
+    }
+    printf("\n");
+    return 0;
+}
