b0f3fc4cc16bd0b1de2fdae9ce3733fa54f31463
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-02-02 08:04:38 +0530

    Add files via upload

---
+++ loops/for loop/assignment_7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,sum=0;
+    for(i=1;i<20;i+=2)
+    {
+        if(i%2!=0)
+    printf("%d\n",i);
+
+    sum+=i;
+
+    }
+    printf("The Sum of odd Natural Numbers upto 10 terms = %d\n",sum);
+    return 0;
+}
