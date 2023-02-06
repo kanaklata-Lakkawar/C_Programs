1da1118cd68ccacf08adbbbbaeb1fba2209c7247
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-02-06 09:44:31 +0530

    Add files via upload
    C program to make such a pattern like right angle triangle with numbers increased by 1

---
+++ loops/pattern/assignment_4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,j,x=0;
+    for(i=1;i<6;i++)
+    {
+        for(j=1;j<i;j++)
+        {
+        printf(" %d",x++);
+        }
+        printf("\n");
+
+    }
+    return 0;
+}
