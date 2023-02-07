dc17d42a98d4d5c4f97993a0cb78ccc5e7d6c9ed
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-02-07 08:21:30 +0530

    Add files via upload
    C program to make such a pattern like a pyramid with numbers increased by 1

---
+++ loops/pattern/assignment_5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,j,k,num=1;
+    for (i=4;i>=1;i--)
+    {
+    for(j=0;j<=i;j++)
+    {
+        printf(" ");
+    }
+    for(k=4;k>=i;k--)
+    {
+        printf(" %d",num++);
+    }
+    printf("\n");
+    }
+
+
+
+    return 0;
+}
