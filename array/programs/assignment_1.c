ebda3865394f8cdd8462b15072996263b9d9420b
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-02-08 10:50:32 +0530

    Add files via upload
    C program to store elements in an array and print it

---
+++ array/programs/assignment_1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i;
+    int a[10]={1,1,2,3,4,5,6,7,8,9};
+    for(i=1;i<10;i++)
+    {
+      printf("%d\t",a[i]);
+    }
+    return 0;
+}
