ed3c2314f30395666e030a1748003dc4c7eebc9e
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-02-10 10:27:08 +0530

    Add files via upload
    C program to find the sum of all elements of the array.

---
+++ array/programs/assignment_3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,sum=0;
+    int a[3]={2,5,8};
+    for(i=0;i<3;i++)
+    {
+        printf("%d\t",a[i]);
+        sum+=a[i];
+    }
+
+    printf("\n sum = %d",sum);
+    return 0;
+}
