6808ddc6347de7ebc96c870799dff7e04b83c868
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-02-11 10:03:45 +0530

    Add files via upload
    C program to find unique element in the array

---
+++ array/programs/assignment_4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i;
+    int a[3]={1,5,1};
+
+    if(a[0]!=a[1] && a[0]!=a[2])
+    {
+        printf("The unique element found in the array is : %d\n",a[0]);
+    }
+     if(a[1]!=a[0] && a[1]!=a[2])
+       {
+            printf("The unique element found in the array is : %d\n",a[1]);
+       }
+       if(a[2]!=a[0] && a[2]!=a[1])
+          {
+                printf("The unique element found in the array is : %d\n",a[2]);
+          }
+
+
+    return 0;
+}
