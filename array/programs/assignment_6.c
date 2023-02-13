2878a7405c1b9e7d23989cfe3f4811c6263f177d
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-02-13 07:42:16 +0530

    Add files via upload
    C program for a 2D array of size 3x3 and print the matrix

---
+++ array/programs/assignment_6.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a[3][3];
+    int i=0,j=0;
+  for(i=0;i<3;i++)
+{
+    for(j=0;j<3;j++)
+     {
+     printf("enter value a[%d][%d]  :",i,j);
+     scanf(" %d",&a[i][j]);
+     }
+  printf("\n");
+}
+
+ for(i=0;i<3;i++)
+{
+      for(j=0;j<3;j++)
+     {
+      printf(" %d ",a[i][j]);
+     }
+  printf("\n");
+}
+    return 0;
+}
