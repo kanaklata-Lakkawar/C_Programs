0a67367fc93e49825ffb6fdf1675ee1f1d205d67
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-02-14 08:12:57 +0530

    Add files via upload
    c program  to input a string and print it

---
+++ string/assignment_1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char c1[9]="Welcome,";
+    char c2[11]="w3resource";
+    printf("The string you entered is : %s %s\n",c1,c2);
+    return 0;
+}
