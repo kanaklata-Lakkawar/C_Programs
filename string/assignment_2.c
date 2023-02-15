98862fd9313d1f4248820a470de2d19604e9e69b
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-02-15 11:55:36 +0530

    Add files via upload
    C program to separate the individual characters from from a string

---
+++ string/assignment_2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char c[15]="w3resource.com";
+    int length=strlen(c);
+    printf("Length of the string is : %d\n",length);
+    return 0;
+}
