f5f20afe16be899732b9928031f560bf45dcf606
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-02-17 09:22:35 +0530

    Update assignment_4.c

--- string/assignment_4.c
+++ string/assignment_4.c
@@ -9,7 +9,7 @@ int main()
     char c[15]="w3resource.com";
     printf("The characters of the string are :");
     int l=sizeof c/sizeof c[0];
-   // printf("%d",l);
+   
     for(int i=0;i<l;i++)
     {
         printf(" %c",c[i]);
