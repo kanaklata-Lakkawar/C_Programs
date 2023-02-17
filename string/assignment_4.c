f0cc4e363b9c451ed9c9630ed723319afe74ca78
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-02-17 09:20:46 +0530

    Add files via upload
    Write C program to print individual characters of string in reverse order

---
+++ string/assignment_4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char c[15]="w3resource.com";
+    printf("The characters of the string are :");
+    int l=sizeof c/sizeof c[0];
+   // printf("%d",l);
+    for(int i=0;i<l;i++)
+    {
+        printf(" %c",c[i]);
+    }
+    printf("\n");
+     printf("The characters of the string in reverse order are :");
+   for(int i=l-1;i>=0;i--)//n/2
+    {
+      printf(" %c",c[i]);
+    }
+
+    return 0;
+}
