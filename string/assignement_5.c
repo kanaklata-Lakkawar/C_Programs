3f91d91dad6b88f19ad3da203c17a24cf75afd3e
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-02-20 11:14:21 +0530

    Update assignement_5.c

--- string/assignement_5.c
+++ string/assignement_5.c
@@ -6,10 +6,10 @@
 
 int main()
 {
-    char ch[50]="This is string mkoji koko";
+    char ch[50]="This is string";
     int count=1;
-    //print("Enter any string");
-    //scanf("%s",sentence)
+    
+    
     for(int i=0;ch[i]!='\0';i++)
     {
         if(ch[i]==' ')
