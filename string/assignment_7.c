31457de3b404c8a1e95443e1ad6936a46874e4f2
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-02-23 11:03:15 +0530

    Add files via upload
    C program to concatenates two strings mannually

---
+++ string/assignment_7.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   char ch1[20]=" this is string one";
+   char ch2[20]=" this is string two";
+   printf("\n Before concatination :\n%s",ch1);
+   printf("\n%s",ch2);
+    strcat(ch1,ch2);
+    printf("\n After Concatination :\n%s",ch1);
+    return 0;
+}
